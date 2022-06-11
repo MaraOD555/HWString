d063124307dcbfee81c8ade4c3f3d2bd1f632297
MaraOD555 <ruffbc@hotmail.com>
2022-06-11 22:32:41 +0300

    Задача 3 - Замена ё на е

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         //четвертая с именем fullName — для хранения Ф. И. О. сотрудника в формате "Фамилия Имя Отчество".
         //Выведите в консоль фразу: “ФИО сотрудника — ….”
         //В качестве данных для задачи используйте “Ivanov Ivan Ivanovich”.
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
 
@@ -21,6 +21,21 @@ public class Main {
 
         String fullNameUpper = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUpper);
+
+        //Задача 3
+        //Нужно написать программу, которая заменяет символ “ё” на символ “е”.
+        //В качестве исходных данных используйте строку fullName и данные в ней “Иванов Семён Семёнович”.
+        //Выведите результат программы в консоль в формате: ”Данные ФИО сотрудника — ...”
+
+        String [] fullName1 = fullName.split (" ");
+        for (int i = 0; i <= fullName1.length-1; i++) {
+            if (fullName1[i].contains("ё")) {
+               fullName1[i] = fullName1[i].replace("ё" , "е");
+            }
+            String fullNameReplace = fullName1[0] + " " + fullName1[1] + " " + fullName1[2];
+            System.out.println();
+            System.out.print("Данные ФИО сотрудника —  " + fullNameReplace);
+        }
 
     }
 }
