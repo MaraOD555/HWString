64b3283d19091b2c917477530959be1440b2af44
MaraOD555 <ruffbc@hotmail.com>
2022-06-11 13:45:20 +0300

    Задача 1 - Строки с ФИО сотрудника

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        //Напишите четыре строки:
+        //первая с именем firstName — для хранения имени;
+        //вторая с именем middleName — для хранения отчества;
+        //третья с именем lastName — для хранения фамилии;
+        //четвертая с именем fullName — для хранения Ф. И. О. сотрудника в формате "Фамилия Имя Отчество".
+        //Выведите в консоль фразу: “ФИО сотрудника — ….”
+        //В качестве данных для задачи используйте “Ivanov Ivan Ivanovich”.
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
     }
 }
